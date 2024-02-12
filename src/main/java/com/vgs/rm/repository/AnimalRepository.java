f672181c837c934ba4d7b18f8289ad39095b8df1
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2024-02-12 17:40:35 -0300

    Criada nova entity.

---
+++ src/main/java/com/vgs/rm/repository/AnimalRepository.java
@@ -0,0 +1,9 @@
+package com.vgs.rm.repository;
+
+import com.vgs.rm.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+}
